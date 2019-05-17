38ecc511cdf3628f093340f254108176be485b87
anamipita <anamipita@gmail.com>
2019-05-17 09:36:12 +0300

    changing port to 5050

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Masaa Ya Noma</h1>
+            <h1 className="App-title">Noma</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
